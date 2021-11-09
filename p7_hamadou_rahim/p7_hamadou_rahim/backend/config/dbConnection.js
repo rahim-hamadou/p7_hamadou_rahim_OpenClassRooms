210bde66d1a75be5ac1046ffcd738608a606e468
rumgalxdead <rumgal@gmail.com>
2021-11-10 00:47:39 +0100

    creation du back end en cours

---
+++ p7_hamadou_rahim/p7_hamadou_rahim/backend/config/dbConnection.js
@@ -0,0 +1,15 @@
+// MODULES
+const mysql = require("mysql");
+const env = require("./environment"); // Récupère les variables d'environnement
+// FIN MODULES
+
+// CONNEXION BASE DE DONNEE
+exports.connection = mysql.createPool({
+	host: "localhost",
+	user: `${env.dbId}`,
+	password: `${env.dbPW}`,
+	database: "groupomania",
+	// timezone: "local",
+	// charset: "utf8mb4",
+});
+// FIN CONNEXION

79501fcf7144db405d0e23ce8c5a68987c219b09
rumgalxdead <rumgal@gmail.com>
2021-11-16 17:27:30 +0100

    Ajouts de la mention admin, et d'info sur la creation du compte adm

--- README.md
+++ README.md
@@ -27,3 +27,15 @@ Installation
 -   Pour démarrer le serveur, placez-vous dans le dossier backend du projet puis exécutez la commande npm install & enfin nodemon server.
 
 -   Ensuite, depuis le dossier frontend du projet, utilisez la commande npm run serve. Rendez-vous ensuite à l'adresse http://localhost:8080/
+
+Creation d'un compte de moderation:
+
+Il faudra penser a creer un compte Admin , qui aura la possibilité de moderé les posts.
+
+-   Creation d'un compte via le site de l'applications
+
+-   Verifification de son id dans la table Users: SELECT \* from Users;
+
+-   Mise a jour de la valeur de isAdmin : UPDATE Users SET isAdmin = '1' WHERE id = "id_du_compte";
+
+Apres cela le compte pourra modérer , une fois connecté.
--- backend/models/user.js
+++ backend/models/user.js
@@ -1,14 +1,19 @@
-'use strict';
+"use strict";
 module.exports = (sequelize, DataTypes) => {
-  const User = sequelize.define('User', {
-    email: DataTypes.STRING,
-    username: DataTypes.STRING,
-    password: DataTypes.STRING,
-    isAdmin: DataTypes.BOOLEAN
-  }, {});
-  User.associate = function(models) {
-    // associations can be defined here
-    models.User.hasMany(models.Post)
-  };
-  return User;
+	const User = sequelize.define(
+		"User",
+		{
+			email: DataTypes.STRING,
+			username: DataTypes.STRING,
+			password: DataTypes.STRING,
+			isAdmin: DataTypes.BOOLEAN,
+			// bio: DataTypes.STRING,
+		},
+		{},
+	);
+	User.associate = function (models) {
+		// associations can be defined here
+		models.User.hasMany(models.Post);
+	};
+	return User;
 };
